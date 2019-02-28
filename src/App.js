5aa2ffc31b56655b7293a2677509bb2880f18373
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-02-28 00:31:22 -0800

    Add ajax request example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./App.css";
+
+const TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json";
 
 const App = props => {
   const [count, setCount] = useState(0);
 
-  const [countMilestone, setCountMilestone] = useState(false);
+  const [milestone, setmilestone] = useState(false);
+  const [topStories, setTopStories] = useState([]);
 
   useEffect(() => {
     document.title = `Counter: ${count}`;
   });
 
   useEffect(() => {
-    if (count % 10 === 0 && count !== 0) setCountMilestone(true);
-    else setCountMilestone(false);
+    if (count % 10 === 0 && count !== 0) setmilestone(true);
+    else setmilestone(false);
   });
+
+  const fetchTopStories = async () => {
+    const response = await axios.get(TOP_STORIES_URL);
+    const allStories = Object.values(response.data);
+    setTopStories(allStories.slice(0, 10));
+  };
+
+  useEffect(() => {
+    fetchTopStories();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-
         <h1>Count: {count}</h1>
 
         <button
@@ -29,7 +42,14 @@ const App = props => {
           Increment Counter
         </button>
 
-        <div>{countMilestone ? "We hit a milestone!" : "Keep going..."}</div>
+        <div>{milestone ? "We hit a milestone!" : "Keep going..."}</div>
+
+        <ul>
+          <h2>Top Stories</h2>
+          {topStories.map(item => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
